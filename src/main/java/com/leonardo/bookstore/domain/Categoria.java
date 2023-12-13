1b86bbe946bc9ca7d7b7b75d40c2e6ec919d3c9b
Leonardo Gabriel <leonardogabrielsilva1@outlook.com>
2023-12-13 11:12:48 -0300

    Domain Create

---
+++ src/main/java/com/leonardo/bookstore/domain/Categoria.java
@@ -0,0 +1,63 @@
+package com.leonardo.bookstore.domain;
+
+import java.util.Objects;
+
+public class Categoria {
+
+	private Integer id;
+	private String nome;
+	private String descricao;
+
+	public Categoria() {
+		super();
+	}
+
+	public Categoria(int id, String nome, String descricao) {
+
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return id == other.id;
+	}
+
+}
