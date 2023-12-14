00552a14df93b7d347345dd24b6772e6e0e3b9bc
Leonardo Gabriel <leonardogabrielsilva1@outlook.com>
2023-12-14 16:56:06 -0300

    excecao personalizada

---
+++ src/main/java/com/leonardo/bookstore/exception/ResourceExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.leonardo.bookstore.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import jakarta.servlet.ServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException e,ServletRequest request){
+		StandardError error = new StandardError(System.currentTimeMillis(),HttpStatus.NOT_FOUND.value(), e.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	//mandar requisição de erro pra requisição
+
+}
--- src/main/java/com/leonardo/bookstore/service/CategoriaService.java
+++ src/main/java/com/leonardo/bookstore/service/CategoriaService.java
@@ -6,17 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.leonardo.bookstore.domain.Categoria;
+import com.leonardo.bookstore.exception.ObjectNotFoundException;
 import com.leonardo.bookstore.repositories.CategoriaRepository;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	public Categoria findByid(Integer id) {
-		 Optional<Categoria> obj = categoriaRepository.findById(id);
-		 return obj.orElse(null); //Optional pois pode ou não encontrar o id
+		Optional<Categoria> obj = categoriaRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado id " + id + "tipo " + Categoria.class.getName())); // Optional pois pode ou não
+		//Caso não ache a categoria devolve minha exessão personalizada																		// encontrar o id
 	}
 
 }
