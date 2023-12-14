2132f06006647b294e974a4e247ba37b2b0f1678
Leonardo Gabriel <leonardogabrielsilva1@outlook.com>
2023-12-14 15:05:27 -0300

    perfil de dev impl

---
+++ src/main/java/com/leonardo/bookstore/config/DevConfig.java
@@ -0,0 +1,28 @@
+package com.leonardo.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.leonardo.bookstore.service.DBservice;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBservice dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")//para subir a base de dados smp que dar deploy 
+	private String strategy;
+
+	@Bean /**sobe o método automaticamente */
+	public boolean instanciaBase() {
+		if(this.strategy.equals("create")){
+			this.dbService.instanciaBaseDeDados();
+		}
+		return false;
+	}
+}
